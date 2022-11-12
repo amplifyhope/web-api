e8a1b283d49e7fc70f1fa507a74ff8abb4a629bd
Anthony Giannell <anthony-giannell@pluralsight.com>
2022-11-11 22:31:13 -0700

    added process exits for docker

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import express from 'express'
 import cors, {CorsOptions} from 'cors'
 import routes from './routes'
 
+process.on('SIGTERM', () => process.exit(0))
+process.on('SIGINT', () => process.exit(0))
 
 const app = express()
 
@@ -12,7 +14,8 @@ const corsOptions: CorsOptions = {
   origin: [
     'https://amplifyhope.cc',
     'https://stage.amplifyhope.cc',
-    'http://dev.amplifyhope.cc:3000'
+    'http://dev.amplifyhope.cc:3000',
+    'http://localhost:3000'
   ]
 }
 
