315f390bf847eaf10580bb9fa998745f15e944df
Anthony Giannell <anthony-giannell@pluralsight.com>
2024-03-28 09:19:21 -0600

    WEB-1 updated portal session endpoint to send the portal object to client instead of redirect

--- src/controllers/portal/create-portal-session.ts
+++ src/controllers/portal/create-portal-session.ts
@@ -12,17 +12,17 @@ export const createPortalSession = async (req: Request, res: Response) => {
 
   try {
     const customer = await stripe.customers.list({ email })
-  
+
     const session = await stripe.billingPortal.sessions.create({
       customer: customer.data[0].id,
-      return_url: req.headers.origin as string
-    });
-  
-    res.redirect(session.url)    
+      return_url: req.headers.origin ?? 'http://localhost:3002'
+    })
+
+    res.status(200).json(session)
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : 'Internal Server error'
+    const errorMessage =
+      error instanceof Error ? error.message : 'Internal Server error'
 
     res.status(500).json({ statusCode: 500, message: errorMessage })
   }
-
 }
